d36dbbc379a4e19d4e3804149813d018ab13001c
Сергей Отенов <serg10.otenov@yandex.ru>
2021-12-07 17:32:16 +0300

    Добавлены классы: Worker, Director, Vice, Employee, Intern, Department, Chief.   Добавлены начальные свойства

---
+++ Home Work 11/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_11
+{
+    abstract class Worker
+    {
+        public string FirstName { get; set; }
+
+        public string Surname  { get; set; }
+
+        public int Id { get; }
+
+        public byte Age { get; set; }
+
+    }
+}
